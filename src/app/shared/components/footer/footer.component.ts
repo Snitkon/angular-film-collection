49511ecd39086f36563afcf207acf019827dc527
Nikita <snitkonikita@gmail.com>
2026-04-30 22:40:32 +0200

    feat: add basic style for header, main and footer block

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -5,5 +5,8 @@ import { Component } from '@angular/core';
   imports: [],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
+  host: {
+    class: 'footer',
+  },
 })
 export class FooterComponent {}
