09d865e1c8cf65750c0924c9a75ffffdca759b8a
Nikita <snitkonikita@gmail.com>
2026-05-01 19:32:40 +0200

    feat: add basic layout search input

--- src/app/features/catalog/catalog.component.ts
+++ src/app/features/catalog/catalog.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { FilmService } from '../../core/services/film.service';
 import { FilmCardComponent } from './components/film-card/film-card.component';
+import { InputComponent } from './components/input/input.component';
 
 @Component({
   selector: 'app-catalog',
-  imports: [FilmCardComponent],
+  imports: [FilmCardComponent, InputComponent],
   templateUrl: './catalog.component.html',
   styleUrl: './catalog.component.scss',
 })
