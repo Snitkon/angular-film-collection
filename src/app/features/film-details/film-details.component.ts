5ed042627359a9fd09e7bcbe848a552ea45b1cb9
Nikita <snitkonikita@gmail.com>
2026-05-04 22:23:01 +0200

    fix: minor fix

--- src/app/features/film-details/film-details.component.ts
+++ src/app/features/film-details/film-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, computed, inject, input } from '@angular/core';
 import { FilmService } from '../../core/services/film.service';
 import { DurationPipe } from '../../shared/pipes/duration.pipe';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-film-details',
@@ -9,10 +10,15 @@ import { DurationPipe } from '../../shared/pipes/duration.pipe';
   styleUrl: './film-details.component.scss',
 })
 export class FilmDetailsComponent {
+  private location = inject(Location);
   private filmService = inject(FilmService);
   public id = input.required<string>();
 
   public filmComputed = computed(() =>
     this.filmService.films().find((film) => film.id === Number(this.id())),
   );
+
+  public back() {
+    this.location.back();
+  }
 }
